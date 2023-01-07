ff3a869091dc64aa470b2980b2aee6344d710355
siopipin <siojurnalispipin@hotmail.com>
2023-01-07 14:03:40 +0700

    simple card

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -17,18 +17,33 @@ class _nameState extends State<Login> {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Text(
-            'data2',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text('data2'),
-        ],
-      ),
+      body: Container(
+          margin: EdgeInsets.all(20),
+          height: 120,
+          color: Colors.blue,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                child: Image.network(
+                  'https://awsimages.detik.net.id/community/media/visual/2021/04/22/5-makanan-enak-dari-indonesia-dan-malaysia-yang-terkenal-enak-5.jpeg?w=700&q=90',
+                  height: 300,
+                  width: 100,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 20),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Title'),
+                      Text('date'),
+                      Text('deskripsi'),
+                    ]),
+              )
+            ],
+          )),
     );
   }
 }
