450d95e583f6a9f3a50ffa40df7440433d05e839
siopipin <siojurnalispipin@hotmail.com>
2023-01-07 13:11:06 +0700

    initial

---
+++ lib/login/login_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _nameState();
+}
+
+class _nameState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Login',
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Text(
+            'data',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text('data2'),
+        ],
+      ),
+    );
+  }
+}
